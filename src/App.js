3cbbbce250d3e2229f0b1c83aacd9614f4a6ce90
michelleejkim <mkim45@uw.edu>
2024-02-29 00:10:28 -0800

    created home page js and added to app.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,54 @@
 import React, {useState} from 'react';
 // import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Navbar from './components/NavBar';
-import SearchBar from './components/SearchBar';
-import Categories from './components/Categories';
-import BoxList from './components/BoxList';
-import CreateForm from './components/CreateForm';
-import './index.css';
+// import Navbar from './components/NavBar';
+// import SearchBar from './components/SearchBar';
+// import Categories from './components/Categories';
+// import BoxList from './components/BoxList';
+// import CreateForm from './components/CreateForm';
+// import './index.css';
+import Indexhome from './components/Indexhome';
+import sample_activities from './data/activities.json';
 
 function App(props) {
 
-  const [activities, setActivities] = useState(props.activities);
-  const [searchQuery, setSearchQuery] = useState('');
-
-  // Update activities based on search query
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-  };
-
   return (
+    
     <div>
-      <Navbar />
-      <SearchBar onSearch={handleSearch}/>
-      <Categories />
-      <section className="trending-spots">
-           <h2>Trending Spots in Seattle</h2>
-      </section>
-
-      <section className="trend-container">
-        <div className="card-container">
-            <div className="container">
-                <div className="row">
-                <BoxList activities={activities} searchQuery={searchQuery} />
-
-                </div>
-                </div>
-                </div>
-                </section>      
+       <Indexhome activities={sample_activities} />
     </div>
+  )
+
+
+  // const [searchQuery, setSearchQuery] = useState('');
+
+  // // Update activities based on search query
+  // const handleSearch = (query) => {
+  //   setSearchQuery(query);
+  // };
+
+  // return (
+  //   <div>
+  //     <Navbar />
+  //     <SearchBar onSearch={handleSearch}/>
+  //     <Categories />
+  //     <section className="trending-spots">
+  //          <h2>Trending Spots in Seattle</h2>
+  //     </section>
+
+  //     <section className="trend-container">
+  //       <div className="card-container">
+  //           <div className="container">
+  //               <div className="row">
+  //               <BoxList activities={activities} searchQuery={searchQuery} />
+
+  //               </div>
+  //               </div>
+  //               </div>
+  //               </section>      
+  //   </div>
     
 
-  );
+  // );
 }
 
 export default App;
---
+++ src/components/Indexhome.js
@@ -0,0 +1,44 @@
+// home page js
+
+import React, {useState} from 'react';
+// import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './NavBar';
+import SearchBar from './SearchBar';
+import Categories from './Categories';
+import BoxList from './BoxList';
+import '../index.css';
+
+export default function Indexhome(props) {
+
+  const [activities, setActivities] = useState(props.activities);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  // Update activities based on search query
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <SearchBar onSearch={handleSearch}/>
+      <Categories />
+      <section className="trending-spots">
+           <h2>Trending Spots in Seattle</h2>
+      </section>
+
+      <section className="trend-container">
+        <div className="card-container">
+            <div className="container">
+                <div className="row">
+                <BoxList activities={activities} searchQuery={searchQuery} />
+
+                </div>
+                </div>
+                </div>
+                </section>      
+    </div>
+    
+
+  );
+}
