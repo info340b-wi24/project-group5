aeec9af52c1b994ff2281485f88e592baa362702
rimarafiq <97317585+rimarafiq@users.noreply.github.com>
2024-03-09 11:10:59 -0800

    build itin interactive

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import sample_activities from './data/activities.json';
 import outdoorsSub from './data/outsideActsubcat.json';
 import totaloutdoors from './data/totaloutsideActs.json';
 import { MySignInScreen } from './components/SignInScreen.js';
+import StartBuilding from './components/StartBuilding.js'; 
+import CreateForm from './components/CreateForm.js'; 
+import ItineraryFormPage2 from './components/ItineraryFormPage2.js'; 
 
 import { AddActivity } from './components/AddActivity';
 import { FinalItinerary } from './components/FinalFinalItinerary';
@@ -17,15 +20,19 @@ import { FinalItinerary } from './components/FinalFinalItinerary';
 function App(props) {
 
   return (
-// <Indexhome />
+ //<Indexhome />
+// <AddActivity></AddActivity>
 //<CreateFormTest />
-<MySignInScreen />
-    // <Router>
-           // <Routes>
-             //   <Route path="/" element={<Indexhome activities={sample_activities} />} />
-              //  <Route path="/categories/:categoryName" element={<CategoriesPage activities={sample_activities} tags={outdoorsSub}/>} />
-          //  </Routes>
-       // </Router>
+//<MySignInScreen />
+     <Router>
+           <Routes>
+               <Route path="/" element={<Indexhome activities={sample_activities} />} />
+               <Route path="/categories/:categoryName" element={<CategoriesPage activities={sample_activities} tags={outdoorsSub}/>} />
+              <Route path="/itinerary" element={<StartBuilding activity_name={'Museum of Pop Culture'} data={{activity_name: 'Museum of Pop Culture', img: "img/mpop.jpeg"}}></StartBuilding>}></Route>
+              <Route path="/itinerary-form" element={<CreateForm></CreateForm>}></Route>
+              <Route path="/itinerary-form-2nd-page" element={<ItineraryFormPage2 activity_name={'Museum of Pop Culture'} data={{activity_name: 'Museum of Pop Culture', img: "img/mpop.jpeg"}}></ItineraryFormPage2>}></Route>
+           </Routes>
+      </Router>
 
 
 
