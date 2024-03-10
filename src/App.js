22d0c305b26e868797cd58998e1b7d55fb73ea8c
rimarafiq <97317585+rimarafiq@users.noreply.github.com>
2024-03-10 09:30:40 -0700

    interactive add-activity

--- src/App.js
+++ src/App.js
@@ -8,29 +8,39 @@ import Indexhome from './components/Indexhome';
 import CategoriesPage from './components/Categoriespage';
 import CreateFormTest from './components/CreateFormTest'
 import sample_activities from './data/activities.json';
-import outdoorsSub from './data/outsideActsubcat.json';
+//import outdoorsSub from './data/outsideActsubcat.json';
 import totaloutdoors from './data/totaloutsideActs.json';
-import subcat from './data/subcat.json';
+//import subcat from './data/subcat.json';
 import { MySignInScreen } from './components/SignInScreen.js';
-import ItineraryFormPage2 from './components/ItineraryFormPage2.js'
+//import ItineraryFormPage2 from './components/ItineraryFormPage2.js'
 import StartBuilding from './components/StartBuilding.js'; 
 import CreateForm from './components/CreateForm.js'; 
 
-import { AddActivity } from './components/AddActivity';
+import { AddActivity } from './components/AddActivity.js';
 import { FinalItinerary } from './components/FinalFinalItinerary';
 
 function App(props) {
 
   return (
 
+    //<CreateFormTest></CreateFormTest>
     <Router>
       <Routes>
-        <Route path="/" element={<Indexhome activities={sample_activities} />} />
-        <Route path="/categories/:categoryName" element={<CategoriesPage activities={sample_activities} tags={subcat}/>} />
-      </Routes>
-    </Router>
-
+        <Route path="/add-activity" element={<AddActivity></AddActivity>}></Route>
+        <Route path="/itinerary-form" element={<CreateForm></CreateForm>}></Route>
+     </Routes>
+   </Router>
+    
   );
+
+   // <Router>
+     // <Routes>
+      //  <Route path="/" element={<Indexhome activities={sample_activities} />} />
+      //  <Route path="/categories/:categoryName" element={<CategoriesPage activities={sample_activities} tags={subcat}/>} />
+     // </Routes>
+  //  </Router>
+
+//  );
 // <Indexhome />
 //<ItineraryFormPage2 />
 // <MySignInScreen />
@@ -69,15 +79,15 @@ function App(props) {
 // <AddActivity></AddActivity>
 //<CreateFormTest />
 //<MySignInScreen />
-     <Router>
-           <Routes>
-               <Route path="/" element={<Indexhome activities={sample_activities} />} />
-               <Route path="/categories/:categoryName" element={<CategoriesPage activities={sample_activities} tags={outdoorsSub}/>} />
-              <Route path="/itinerary" element={<StartBuilding activity_name={'Museum of Pop Culture'} data={{activity_name: 'Museum of Pop Culture', img: "img/mpop.jpeg"}}></StartBuilding>}></Route>
-              <Route path="/itinerary-form" element={<CreateForm></CreateForm>}></Route>
-              <Route path="/itinerary-form-2nd-page" element={<ItineraryFormPage2 activity_name={'Museum of Pop Culture'} data={{activity_name: 'Museum of Pop Culture', img: "img/mpop.jpeg"}}></ItineraryFormPage2>}></Route>
-           </Routes>
-      </Router>
+    // <Router>
+      //     <Routes>
+       //        <Route path="/" element={<Indexhome activities={sample_activities} />} />
+       //        <Route path="/categories/:categoryName" element={<CategoriesPage activities={sample_activities} tags={outdoorsSub}/>} />
+       //       <Route path="/itinerary" element={<StartBuilding activity_name={'Museum of Pop Culture'} data={{activity_name: 'Museum of Pop Culture', img: "img/mpop.jpeg"}}></StartBuilding>}></Route>
+        //      <Route path="/itinerary-form" element={<CreateForm></CreateForm>}></Route>
+        //      <Route path="/itinerary-form-2nd-page" element={<ItineraryFormPage2 activity_name={'Museum of Pop Culture'} data={{activity_name: 'Museum of Pop Culture', img: "img/mpop.jpeg"}}></ItineraryFormPage2>}></Route>
+      //     </Routes>
+  //    </Router>
 
 
 
