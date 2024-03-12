e5dda038aae0ccd05a53630769ea13d9d70289f9
michelleejkim <mkim45@uw.edu>
2024-03-12 14:44:48 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -7,22 +7,11 @@ import Indexhome from './components/Indexhome';
 import CategoriesPage from './components/Categoriespage';
 import CreateFormTest from './components/CreateFormTest'
 import sample_activities from './data/activities.json';
-<<<<<<< HEAD
 import { MySignInScreen } from './components/SignInScreen.js';
 
 import subcat from './data/subcat.json';
 //import { MySignInScreen } from './components/SignInScreen.js';
 //import ItineraryFormPage2 from './components/ItineraryFormPage2.js'
-=======
-// import outdoorsSub from './data/outsideActsubcat.json';
-//import totaloutdoors from './data/totaloutsideActs.json';
-// import subcat from './data/subcat.json';
-import SignInScreen from './components/SignInScreen.js';
-
-//import outdoorsSub from './data/outsideActsubcat.json';
-import totaloutdoors from './data/totaloutsideActs.json';
-//import subcat from './data/subcat.json';
->>>>>>> 0aa0472a4d50cbe4922371879726b6d45c57cff1
 import StartBuilding from './components/StartBuilding.js'; 
 import ItineraryFormPage from './components/ItineraryFormPage.js';
 import ActivityReview from './components/ActivityReview.js';
@@ -33,16 +22,15 @@ import { FinalItinerary } from './components/FinalFinalItinerary';
 function App(props) {
 
   return (
-<<<<<<< HEAD
     //michelle's 
-    <Router>
-    <Routes>
-        <Route path="/" element={<Indexhome activities={sample_activities} />} />
-        <Route path="/activities/:activity_name" element={<ActivityReview activities={sample_activities} />} />
-        <Route path="/categories/:categoryName" element={<CategoriesPage activities={sample_activities} tags={subcat}/>} />
-       <Route path="/itinerary" element={<StartBuilding activity_name={'Museum of Pop Culture'} data={{activity_name: 'Museum of Pop Culture', img: "img/mpop.jpeg"}}></StartBuilding>}></Route>
-      </Routes>
-</Router>
+//     <Router>
+//     <Routes>
+//         <Route path="/" element={<Indexhome activities={sample_activities} />} />
+//         <Route path="/activities/:activity_name" element={<ActivityReview activities={sample_activities} />} />
+//         <Route path="/categories/:categoryName" element={<CategoriesPage activities={sample_activities} tags={subcat}/>} />
+//        <Route path="/itinerary" element={<StartBuilding activity_name={'Museum of Pop Culture'} data={{activity_name: 'Museum of Pop Culture', img: "img/mpop.jpeg"}}></StartBuilding>}></Route>
+//       </Routes>
+// </Router>
 
 
   //   <Router>
@@ -53,18 +41,6 @@ function App(props) {
   //       <Route></Route>
   //    </Routes>
   //  </Router>
-=======
-
-      <Routes>
-        <Route path="/" element={<Indexhome activities={sample_activities} />} />
-        <Route path="/signin" element={<SignInScreen></SignInScreen>}></Route>
-        <Route path="/add-activity" element={<AddActivity></AddActivity>}></Route>
-        <Route path="/final-itin" element={<FinalItinerary></FinalItinerary>}></Route>
-        <Route path ="/itinerary-form" element={<CreateFormTest></CreateFormTest>}></Route>
-        <Route path="/start-building" element={<StartBuilding></StartBuilding>}></Route>
-     </Routes>
-  
->>>>>>> 0aa0472a4d50cbe4922371879726b6d45c57cff1
 
 
   /*   
